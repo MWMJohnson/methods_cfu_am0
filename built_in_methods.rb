594f23bf15ccf818ce69b09ac66e29c8d54ad26c
Matthew Johnson <matthewwmjohnson@gmail.com>
2023-02-23 20:10:52 -0700

    Add final_practice.rb content

--- built_in_methods.rb
+++ built_in_methods.rb
@@ -6,22 +6,25 @@
 # The downcase method is called on the string object "Hello World"
 # No arguments are passed; downcase has one clear job which is to lowercase all letters that exist in the String
 # The return value is "hello world"
-"Hello World".downcase
+
+p "Hello World".downcase
+#  Calling on a built-in-method for a string variable to return a new string value changing all lowercase characters to uppercase. Although this is not a permanent change.
+
+p "Hello World".include?("Hello")
+#  Calling on a built-in-method for a string variable to return a boolean value confirming if our argument,"Hello", is included in the string variable anywhere. If yes then true, if no then false. Our feedback is true.
 
 
-"Hello World".include?("Hello")
+p "Hello World".end_with?("Hello")
+#  Calling on a built-in-method for a string variable to return a boolean value confirming if our argument,"Hello", matches the exact characters placed at the end of the string for the number of characters in the argument. If yes then true, if no then false. Our feedback is false
 
-
-"Hello World".end_with?("Hello")
-
-
-"Hello World".end_with?("rld")
-
+p "Hello World".end_with?("rld")
+# Calling on a built-in-method for a string variable to return a boolean value confirming if our argument,"Hello", matches the exact characters placed at the end of the string for the number of characters in the argument. If yes then true, if no then false. Our feedback is true
 
 12.even?
-
+# This method allows us to enter a integer as our argumnent in the parameter, and ruby evaluates if the integer is even resuting in a boolean response, with yes as true and no as false.
 
 18.next
+# the method allows you to iterate through a list while only allowing certain elements to receive instruction or coding. Next will bypass the coding for an element if that element already fulfills the conditions of true.
 
 
 
@@ -30,6 +33,20 @@
 # Call a different built-in Ruby method on each of your variables. 
 # https://ruby-doc.org/core-3.1.0/String.html
 # Include comments above each method call explaining the impact and return value of that method.
+
+# capitalize method
+#  Impact: It will convert the first character of the string object in our argument to uppercase, with a return value "Bob davis"
+# Return Value: "Bob davis", although you will only see this if using the puts command.
+name = "bob davis"
+p name.capitalize
+
+# count method
+# This method can use one or multiples characters as parameters to count the number of matching characters in a string object. By using the puts command, we would receive an integer as our feedback confirming the number of appearances.
+# Return Value of this method will be the number of times we have a matching character in our string compared to our argument. Using "Z" as our argument the return value is 1.
+name = "Zoolander"
+p name.count("Z")
+
+
 
 # EXAMPLE
 # The start_with? method is called on the first_name variable, which stores the string object "Jeff". 
@@ -47,6 +64,17 @@ puts first_name.start_with?("J")
 # https://ruby-doc.org/core-3.1.0/Integer.html
 # Include comments above each method call explaining the impact and return value of that method.
 
+# magnitude method
+# In this example, the magnitude method is called upon by the integer variable "temperature" to give an absolute value of the argument or its self.
+# Using the puts command we see a return value of 13.
+temperature = -13
+p temperature.abs
+
+# remainder method
+# In this example speed is an assigned integer variable to 40 calling the remainder method, this method will calculate the remainder after dividing the data in the speed variable by the argument.
+# It gives us a return value of 10 and we can see this on the console using the puts command.
+speed = 40
+p speed.remainder(30)
 
 
 # SECTION 4: Calling methods on variables assigned to arrays.
@@ -54,3 +82,19 @@ puts first_name.start_with?("J")
 # Call a different built-in Ruby method on each of your variables. 
 # https://ruby-doc.org/core-3.1.0/Array.html
 # Include comments above each method call explaining the impact and return value of that method.
+
+# drop method
+# This method is similiar to the shift method however instead of returing the separated element from the array, the return value is the new array without the previous elements.
+# The drop method will remove elements starting at the front of the array and will remove the number of elements that matches the argument. This specific example removes the first two elements of the array since 2 is our argument.
+age_group = [1, 3, 5, 8, 9, 11, 12]
+p age_group
+p age_group.drop(2)
+
+
+# join method
+# this method returns a string value combines the elements of a string array into one string. The array is not permanently impacted, but the output will one string.
+# In this specific example when calling the join method the puts command will display on the console a string value "tigermonkeyzebra".
+animals = ["tiger", "monkey", "zebra"]
+p animals.join
+
+
--- final_practice.rb
+++ final_practice.rb
@@ -6,34 +6,77 @@
 
 # 1: Write a method named greeting that returns a string with a general greeting. 
 
-# What is the return value of your method?
-# How many arguments did you pass your method?
+# What is the return value of your method? "Hello,person"
+# How many arguments did you pass your method? 0
+
+def greeting
+    "Hello, person."
+end
+
+p greeting
 
 
 # 2: Write a method named custom_greeting that returns a greeting WITH a specific name.
 
-# What is the return value of your method?
-# How many arguments did you pass your method?
-# What data type was your argument(s)?
+# What is the return value of your method? "hello Bob." an interpolated string matching the argument
+# How many arguments did you pass your method? 1
+# What data type was your argument(s)? string
+
+def hello(name)
+    p "Hello #{name}."
+end
+
+hello("Bob")
+hello("Steve")
 
 
 # 3: Write a method named greet_person that takes in 3 strings, a first, middle, and last name, and returns a sentence with the full name.
 
-# What is the return value of your method?
-# How many arguments did you pass your method?
-# What data type was your argument(s)?
+# What is the return value of your method? "Welcome Joe Bob Smith" an interpolated string matching the arguments
+# How many arguments did you pass your method? 3
+# What data type was your argument(s)? string
+
+def greet_person(first, middle, last)
+    p "Welcome #{first} #{middle} #{last}."
+end
+
+greet_person("Joe", "Bob", "Smith")
+greet_person("Sara", "Jill", "Susan")
+
 
 
 # 4: Write a method named square that takes in one integer, and returns the square of that integer.
 # Bonus: Print a sentence that interpolates the return value of your square method.
 
-# What is the return value of your method?
-# How many arguments did you pass your method?
-# What data type was your argument(s)?
+# What is the return value of your method? the square of the integer we input
+# How many arguments did you pass your method? 1
+# What data type was your argument(s)?integer
+
+def square(num)
+    num * num
+end
+
+p square(25)
+p square (100)
+
 
 
 # 5: Write a method named check_stock that satisfies the following interaction pattern:
 # Hint: You will only write one check_stock method that checks the quantity and then prints the corresponding statement.
+
+def check_stock(number, food)
+    if number == 0
+        puts "#{food} - OUT of stock!"
+    elsif
+        number <= 3 && number > 0
+        puts "#{food} - running LOW" 
+    else
+        puts "#{food} is stocked"
+    end
+end
+
+        
+    
 
 check_stock(4, "Coffee");
 # => "Coffee is stocked"
